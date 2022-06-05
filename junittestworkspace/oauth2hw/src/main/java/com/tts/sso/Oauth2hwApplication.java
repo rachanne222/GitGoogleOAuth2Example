331e1ac3c285f0c00407ef809c50c38d8ef2ed5d
rachanne22 <rachel.sickle@gmail.com>
2022-06-05 16:54:11 -0400

    GitHub and Google authentication w/o authentication failure detection and error reporting

---
+++ junittestworkspace/oauth2hw/src/main/java/com/tts/sso/Oauth2hwApplication.java
@@ -0,0 +1,13 @@
+package com.tts.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2hwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2hwApplication.class, args);
+	}
+
+}
