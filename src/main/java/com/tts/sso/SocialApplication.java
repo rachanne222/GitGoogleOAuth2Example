4adb92aeb319fe6136deede0dd8aff2a2fbb1d21
rachanne22 <rachel.sickle@gmail.com>
2022-06-05 17:27:04 -0400

    partial hw

---
+++ src/main/java/com/tts/sso/SocialApplication.java
@@ -0,0 +1,106 @@
+package com.tts.sso;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.util.Instantiator.FailureHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SocialApplication extends WebSecurityConfigurerAdapter {
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+//    @GetMapping("/error")
+//    public String error(HttpServletRequest request) {
+//    	String message = (String) request.getSession().getAttribute("error.message");
+//    	request.getSession().removeAttribute("error.message");
+//    	return message;
+//    }
+    protected void configure(HttpSecurity http) throws Exception {
+    	// @formatter:off
+        http
+            .authorizeRequests(a -> a
+                .antMatchers("/", "/error", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .csrf(c -> c
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
+            .exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            )
+            .oauth2Login()
+        .and()
+        .logout(l -> l
+                .logoutSuccessUrl("/").permitAll()
+            );
+//       .oauth2Login(o -> o
+//                .failureHandler((request, response, exception) -> {
+//    			    request.getSession().setAttribute("error.message", exception.getMessage());
+//    			    handler.onAuthenticationFailure(request, response, exception);
+//                })
+//            );;
+        
+        
+        // @formatter:on
+    }
+    
+//    @Bean
+//    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService(WebClient rest) {
+//        DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+//        return request -> {
+//            OAuth2User user = delegate.loadUser(request);
+//            if (!"github".equals(request.getClientRegistration().getRegistrationId())) {
+//            	return user;
+//            }
+//
+//            OAuth2AuthorizedClient client = new OAuth2AuthorizedClient
+//                    (request.getClientRegistration(), user.getName(), request.getAccessToken());
+//            String url = user.getAttribute("organizations_url");
+//            List<Map<String, Object>> orgs = rest
+//                    .get().uri(url)
+//                    .attributes(client)
+//                    .retrieve()
+//                    .bodyToMono(List.class)
+//                    .block();
+//
+//            if (orgs.stream().anyMatch(org -> "spring-projects".equals(org.get("login")))) {
+//                return user;
+//            }
+//
+//            throw new OAuth2AuthenticationException(new OAuth2Error("invalid_token", "Not in Spring Team", ""));
+//        };
+//    }
+    
+    @GetMapping("/error")
+ 
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tts/sso/Oauth2hwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2hwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
